c2900c39582a2da34cb85e781a60d1e322bec1db
jennyfertellez <jennyfertellez@gmail.com>
2022-12-22 23:40:14 -0800

    update the info on create course and downloaded the react markdown

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -4,40 +4,121 @@ import { useNavigate } from 'react-router-dom';
 //This component provides the "Create a Course" screen that allows users to create a course
 const CreateCourse = ({ context }) => {
     //sets references & states for the objects that are required to build the course object 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [estimatedTime, setEstimatedTime] = useState("");
-    const [materialsNeeded, setMaterialsNeeded] = useState("");
-    const [errors, setErrors] = useState([]);
+    const title = useRef();
+    const description = useRef();
+    const estimatedTime = useRef();
+    const materialsNeeded = useRef();
 
     const navigate = useNavigate();
+    const authUser = context.authenticatedUser;
+    const [errors, setErrors] = useState([]);
 
     //Function that handles input changes 
     const handleChange = (e) => {
-        e.preventDefault();
-
-        const name = e.target.name;
-        const value = e.target.value;
-
-        if (name === "courseTitle") {
-            setTitle(value);
-          } else if (name === "courseDescription") {
-            setDescription(value);
-          } else if (name === "estimatedTime") {
-            seteEtimatedTime(value);
-          } else if (name === "materialsNeeded") {
-            setMaterialsNeeded(value);
+      e.preventDefault();
+      
+      const course = {
+        userId: context.authenticatedUser.id,
+        title: title.current.value,
+        description: description.current.value,
+        estimatedTime: estimatedTime.current.value,
+        materialsNeeded: materialsNeeded.current.value,
+      };
+      context.data
+        .createCourse(
+          course,
+          context.authenticatedUser.email,
+          context.authenticatedUser.password
+        )
+        .then((errors) => {
+          if (errors.length) {
+            setErrors(errors);
           } else {
-            return;
+            navigate("/");
           }
-        };
-    
-    const handleSubmit = async (e) => {
-        //prevents buttons from automatically submitting
+        })
+        .catch((err) => {
+          console.log(err);
+          navigate("/error");
+        });
+    };
+//The cancel button reroutes the user to the course detail page 
+    const handleCancel = async (e) => {
+        //prevents button from automatically submitting
         e.preventDefault();
-    }
-    }
+        navigate("/");
+    };
 
 //This component also renders a "Create a Course" button that sends a POST request to the REST API's route
-
 //This component also renders a "Cancel" button that returns the user to the default route
+return (
+    <main>
+      <div className="wrap">
+        <h2>Create Course</h2>
+        {errors && errors.length ? (
+          <div className="validation--errors">
+            <h3>Validation Errors</h3>
+            <ul>
+              {errors.map((error, index) => (
+                <li key={index}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+        <form onSubmit={handleCreateCourse}>
+          <div className="main--flex">
+            <div>
+              <label htmlFor="courseTitle">Course Title</label>
+              <input
+                id="courseTitle"
+                name="courseTitle"
+                type="text"
+                defaultValue=""
+                ref={title}
+              />
+
+              <p>
+                By {authUser.firstName} {authUser.lastName}
+              </p>
+
+              <label htmlFor="courseDescription">Course Description</label>
+              <textarea
+                id="courseDescription"
+                name="courseDescription"
+                defaultValue=""
+                ref={description}
+              ></textarea>
+            </div>
+            <div>
+              <label htmlFor="estimatedTime">Estimated Time</label>
+              <input
+                id="estimatedTime"
+                name="estimatedTime"
+                type="text"
+                defaultValue=""
+                ref={estimatedTime}
+              />
+
+              <label htmlFor="materialsNeeded">Materials Needed</label>
+              <textarea
+                id="materialsNeeded"
+                name="materialsNeeded"
+                defaultValue=""
+                ref={materialsNeeded}
+              ></textarea>
+            </div>
+          </div>
+          <button className="button" type="submit">
+            Create Course
+          </button>
+
+          <button onClick={handleCancel} className="button button-secondary">
+            Cancel
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default CreateCourse;
