851bfc82634cb7c3d28022bbb039f8f7a6032269
jennyfertellez <jennyfertellez@gmail.com>
2022-12-23 02:32:33 -0800

    create the app and data file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom"
-import axios from "axios";
+import axios from 'axios'
+import withContext from "./Context";
 
 //----Components -------
 import Header from "./components/Header";
@@ -12,19 +13,40 @@ import UserSignIn from "./components/UserSignIn";
 import UserSignUp from "./components/UserSignUp";
 import UserSignOut from "./components/UserSignOut";
 
+//------ Components with Context -------//
+const HeaderWithContext = withContext(Header);
+const CoursesWithContext = withContext(Courses);
+const CourseDetailWithContext = withContext(CourseDetail);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignOutWithContext = withContext(UserSignOut);
+
+
 
 const App = () => {
-  let courses = [];  
-  axios.get('http://localhost:5000/api/courses')
-    .then((response) => {
-      courses = response.data;
-    }
-  );
   return (
-    <div>
-      courses.
-    </div>
+    <React.Fragment>
+      <HeaderWithContext />
+      <Routes>
+        <Route element={<PrivateRoutesWithContext />}>
+          <Route
+            path="/courses/:id/update"
+            element={<UpdateCourseWithContext />}
+          />
+          <Route path="/courses/create" element={<CreateCourseWithContext />} />
+        </Route>
+        <Route path="/" element={<CoursesWithContext />} />
+        <Route path="/signin" element={<UserSignInWithContext />} />
+        <Route path="/signup" element={<UserSignUpWithContext />} />
+        <Route path="/signout" element={<UserSignOutWithContext />} />
+        <Route path="/courses" element={<CoursesWithContext />} />
+        <Route path="/courses/:id" element={<CourseDetailWithContext />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ client/src/Data.js
@@ -0,0 +1,128 @@
+import config from "./config";
+
+// GET, POST, DELETE, and PUT requests to the REST API
+export default class Data {
+  api(
+    path,
+    method = "GET",
+    body = null,
+    requiresAuth = false,
+    credentials = null
+  ) {
+    const url = config.apiBaseUrl + path;
+
+    const options = {
+      method,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    };
+
+    if (body !== null) {
+      options.body = JSON.stringify(body);
+    }
+    // Checks if authorization is required
+    if (requiresAuth) {
+      const encodedCredentials = btoa(
+        `${credentials.emailAddress}:${credentials.password}`
+      );
+      options.headers["Authorization"] = `Basic ${encodedCredentials}`;
+    }
+
+    return fetch(url, options);
+  }
+
+  // GET User from the database
+  async getUser(emailAddress, password) {
+    const response = await this.api(`/users`, "GET", null, true, {
+      emailAddress,
+      password,
+    });
+    if (response.status === 200) {
+      return response.json().then((data) => data);
+    } else if (response.status === 401) {
+      return null;
+    } else {
+      throw new Error();
+    }
+  }
+
+// Creates User
+    async createUser(user) {
+        const response = await this.api("/users", "POST", user);
+        if (response.status === 201) {
+          return [];
+        } else if (response.status === 400) {
+          return response.json().then((data) => {
+            return data.errors;
+          });
+        } else {
+          throw new Error();
+        }
+     }
+ // GET all courses from the database
+  async getCourses() {
+    const response = await this.api(`/courses`, "GET", null);
+    if (response.status === 200) {
+      return response.json().then((data) => data);
+    } else {
+      throw new Error();
+    }
+  }
+  // GET a specific course
+  async getCourse(id) {
+    const response = await this.api(`/courses/${id}`, "GET", null);
+    if (response.status === 200) {
+      return response.json().then((data) => data);
+    } else {
+      throw new Error();
+    }
+  }
+  // Creates Course
+  async createCourse(body, emailAddress, password) {
+    const response = await this.api("/courses", "POST", body, true, {
+      emailAddress,
+      password,
+    });
+    if (response.status === 201) {
+      return [];
+    } else if (response.status === 400) {
+      return response.json().then((data) => {
+        return data.errors;
+      });
+    } else {
+      throw new Error();
+    }
+  }
+
+  // Updates a Course
+  async updateCourse(id, body, emailAddress, password) {
+    const response = await this.api(`/courses/${id}`, "PUT", body, true, {
+      emailAddress,
+      password,
+    });
+    if (response.status === 204) {
+      return [];
+    } else if (response.status === 400) {
+      return response.json().then((data) => {
+        return data.errors;
+      });
+    } else {
+      throw new Error();
+    }
+  }
+  // DELETE a Course (with authorization)
+  async deleteCourse(id, emailAddress, password) {
+    const response = await this.api(`/courses/${id}`, "DELETE", null, true, {
+      emailAddress,
+      password,
+    });
+    if (response.status === 204) {
+      return [];
+    } else if (response.status === 401) {
+      return null;
+    } else {
+      throw new Error();
+    }
+  }
+}
